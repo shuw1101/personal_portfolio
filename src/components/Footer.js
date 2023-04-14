e0734b35d16c25c892cb1cedeae8bb190f84e997
Arella1101 <guansw971101@163.com>
2023-04-14 15:58:54 -0700

    update foot components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
-import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import linkedin from '../assets/img/linkedin.png';
+import github from '../assets/img/github.png';
+
 
 export const Footer = () => {
   return (
@@ -16,11 +16,11 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="https://www.linkedin.com/in/shuwenguan/"><img src={navIcon1} alt="Icon" /></a>
-              {/* <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a> */}
+              <a href="https://www.linkedin.com/in/shuwenguan/"><img src={linkedin} alt="Icon" /></a>
+              <a href="https://github.com/shuw1101/personal_portfolio"><img src={github} alt="" /></a>
+
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright 2023. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
