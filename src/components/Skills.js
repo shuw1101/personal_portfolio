20d663cb9ee14bce06cd09c45002a0bb179d8a68
Arella1101 <guansw971101@163.com>
2023-05-02 20:26:24 -0700

    update projects

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -32,7 +32,7 @@ export const Skills = () => {
   };
 
   return (
-    <section className="skill" id="skills">
+    <section className="skill" id="interests">
         <div className="container">
             <div className="row">
                 <div className="col-12">
